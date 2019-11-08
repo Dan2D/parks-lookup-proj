d4d27c698bd264e964b08da2a0252c3f028dace1
Dan Schultz <travelguy90@gmail.com>
2019-11-08 12:51:31 -0800

    (update): Added additional functionality to park summary/map markers

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -7,7 +7,20 @@ function MapContainer({parks, statePos, zoom, }) {
 
     function handleMarkerClick(num) {
         let btn = document.querySelector(`label.btn-expand-label${num}`);
+        window.scrollTo(0, btn.parentElement.offsetTop);
         btn.click();
+    }
+
+    function handleMarkerHover(num, event) {
+        console.log(num, event)
+        let toolTip = document.querySelector(`.tooltip-${num}`);
+        console.log(toolTip)
+        if (event === 'enter'){
+            toolTip.style.display = 'block';
+        }
+        else {
+            toolTip.style.display = 'none';
+        }
     }
 
     let markerArr=[];
@@ -17,16 +30,21 @@ function MapContainer({parks, statePos, zoom, }) {
                 iconSize: null, 
                 iconAnchor: [40, 22],
                 tooltipAnchor: [0, 0],
-                popupAnchor: [-27, -20],
+                // popupAnchor: [-27, -20],
                 html: `<span class='marker-${ind+1}'>${parseInt(ind+1)}</span>` });
         const PARK_NAME = parks[key].name;
         const COORD = formatCoord(parks[key].latLong);
         if (COORD){
             markerArr.push(
-                <Marker key={key} position={COORD} icon={numIcon} onClick={() => handleMarkerClick(ind+1)}>
-                    <Popup>
+                <Marker key={key}  
+                        position={COORD} 
+                        icon={numIcon} 
+                        onClick={() => handleMarkerClick(ind+1)} 
+                        onMouseOver={() => handleMarkerHover(ind + 1, 'enter')} 
+                        onMouseOut={() => handleMarkerHover(ind + 1, 'leave')}>
+                    {/* <Popup>
                         <span>{PARK_NAME}</span>
-                    </Popup>
+                    </Popup> */}
                     <Tooltip className={`tooltip-${ind+1}`} direction='left'  permanent={true}>
                         {PARK_NAME}
                     </Tooltip>
