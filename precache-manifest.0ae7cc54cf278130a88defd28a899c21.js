4a418caffc7408f50848f8f276dbd33aab634c55
Dan Schultz <travelguy90@gmail.com>
2019-11-11 14:44:47 -0800

    Updates

---
+++ precache-manifest.0ae7cc54cf278130a88defd28a899c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22304f677719908079b166a1280db76e",
+    "url": "fonts/icofont.ttf"
+  },
+  {
+    "revision": "0be8257bbf7f5bf4c09c67547da40d7a",
+    "url": "index.html"
+  },
+  {
+    "revision": "246ddde2f1543a6b6a8a",
+    "url": "main.6a2c19856eecc4f9c54f.bundle.js"
+  },
+  {
+    "revision": "77382db51e895f9e75f0315e94014c1a",
+    "url": "main.6a2c19856eecc4f9c54f.bundle.js.br"
+  },
+  {
+    "revision": "246ddde2f1543a6b6a8a",
+    "url": "main.dba8829ffa73faaf6c4c.css"
+  },
+  {
+    "revision": "5f767d073dbcf82adf6bff0da4a9b1cd",
+    "url": "main.dba8829ffa73faaf6c4c.css.br"
+  },
+  {
+    "revision": "cd71d00953375017666372044d37c09c",
+    "url": "site.manifest"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.0ae7cc54cf278130a88defd28a899c21.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(png|jpg|jpeg|svg)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 15, maxAgeSeconds: 604800, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/https:\/\/fonts.googleapis.com\/css?family=Oxygen:300,400&display=swap/, new workbox.strategies.StaleWhileRevalidate({ plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
